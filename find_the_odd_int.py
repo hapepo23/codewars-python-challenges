ddd2778e3095640d59499e4d7dbc02cca1cf81ea
hapepo23 <238172935+hapepo23@users.noreply.github.com>
2026-03-16 14:21:42 +0100

    update 2026-03-16 14:21 CET

--- find_the_odd_int.py
+++ find_the_odd_int.py
@@ -5,58 +5,53 @@ Find the odd int
 https://www.codewars.com/kata/54da5a58ea159efa38000836
 '''
 
-
 # ---- FIRST SOLUTION --------------------------------------------------
 
 def find_it(seq):
-	counts = {}
-	for num in seq:
-		counts[num] = counts.get(num, 0) + 1
-	for k, v in counts.items():
-		if v % 2 == 1:
-			return k;
-	return 0  # should not happen
-
+  counts = {}
+  for num in seq:
+    counts[num] = counts.get(num, 0) + 1
+  for k, v in counts.items():
+    if v % 2 == 1:
+      return k;
+  return 0  # should not happen
 
 # ---- SECOND SOLUTION -------------------------------------------------
-
+'''
 # XOR has useful properties: a ^ a = 0 and a ^ 0 = a
 # So all paired numbers cancel out: a ^ a ^ b ^ b ^ c = c
 
-# def find_it(seq):
-# 	result = 0
-# 	for num in seq:
-# 		result ^= num
-# 	return result
-
+def find_it(seq):
+  result = 0
+  for num in seq:
+    result ^= num
+  return result
+'''
 
 # ---- THIRD SOLUTION --------------------------------------------------
-
+'''
 # Even shorter using functools.reduce
 
-# from functools import reduce
-# from operator import xor
-#
-# def find_it(seq):
-# 	return reduce(xor, seq)
+from functools import reduce
+from operator import xor
 
+def find_it(seq):
+  return reduce(xor, seq)
+'''
 
 def dotest(seq, expected):
-	actual = find_it(seq)
-	status = 'OK' if expected == actual else 'FAIL'
-	print(f'seq = {seq}, expected = {expected}, actual = {actual} -> {status}')
-
+  actual = find_it(seq)
+  status = 'OK' if expected == actual else 'FAIL'
+  print(f'seq = {seq}, expected = {expected}, actual = {actual} -> {status}')
 
 def main():
-	dotest([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5], 5)
-	dotest([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5], -1);
-	dotest([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5], 5);
-	dotest([10], 10);
-	dotest([10, 10, 10], 10);
-	dotest([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1], 10);
-	dotest([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10], 1);
-	dummy = input('Press ENTER ...')
-
+  dotest([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5], 5)
+  dotest([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5], -1);
+  dotest([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5], 5);
+  dotest([10], 10);
+  dotest([10, 10, 10], 10);
+  dotest([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1], 10);
+  dotest([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10], 1);
 
 if __name__ == "__main__":
-	main()
+  main()
--- neutralisation.py
+++ neutralisation.py
@@ -5,66 +5,61 @@ Neutralisation
 https://www.codewars.com/kata/65128732b5aff40032a3d8f0
 '''
 
-
 # ---- FIRST SOLUTION --------------------------------------------------
 
 def neutralise(s1, s2):
-	r = ''
-	for i in range(0, len(s1)):
-		if s1[i] != s2[i]:
-			r += '0'
-		else:
-			r += s1[i]
-	return r
-
+  r = ''
+  for i in range(0, len(s1)):
+    if s1[i] != s2[i]:
+      r += '0'
+    else:
+      r += s1[i]
+  return r
 
 # ---- SECOND SOLUTION -------------------------------------------------
-
-# def neutralise(s1, s2):
-# 	r = ''
-# 	for i, c in enumerate(s1):
-# 		r += '0' if c != s2[i] else c
-# 	return r
-
+'''
+def neutralise(s1, s2):
+  r = ''
+  for i, c in enumerate(s1):
+    r += '0' if c != s2[i] else c
+  return r
+'''
 
 # ---- THIRD SOLUTION --------------------------------------------------
-
-# def neutralise(s1, s2):
-# 	r = ''
-# 	for a, b in zip(s1, s2):
-# 		r += '0' if a != b else a
-# 	return r
-
+'''
+def neutralise(s1, s2):
+  r = ''
+  for a, b in zip(s1, s2):
+    r += '0' if a != b else a
+  return r
+'''
 
 def dotest(s1, s2, expected):
-	actual = neutralise(s1, s2)
-	status = 'OK' if expected == actual else 'FAIL'
-	print(f's1 = {s1}, s2 = {s2}, expected = {expected}, actual = {actual} -> {status}')
-
+  actual = neutralise(s1, s2)
+  status = 'OK' if expected == actual else 'FAIL'
+  print(f's1 = {s1}, s2 = {s2}, expected = {expected}, actual = {actual} -> {status}')
 
 def main():
-	dotest("--++--", "++--++", "000000")
-	dotest("-+-+-+", "-+-+-+", "-+-+-+")
-	dotest("-++-", "-+-+", "-+00")
-	dotest("--++", "++++", "00++")
-	dotest("+++--+---", "++----++-", "++0--000-")
-	dotest("-----", "-----", "-----")
-	dotest("-+", "++", "0+")
-	dotest("--", "-+", "-0")
-	dotest("-++", "+--", "000")
-	dotest("++-++--++-", "-+++-++-++", "0+0+0000+0")
-	dotest("-++-+-++-", "+-++++---", "00+0+000-")
-	dotest("---++-+--", "-+++--++-", "-00+0-+0-")
-	dotest("+-----+++-", "--+-+-++--", "0-0-0-++0-")
-	dotest("+-----+-", "---++-++", "0--00-+0")
-	dotest("-+--+-+---", "-+--+-+-+-", "-+--+-+-0-")
-	dotest("+-+", "-++", "00+")
-	dotest("-++", "-+-", "-+0")
-	dotest("---+", "-+++", "-00+")
-	dotest("+--", "+--", "+--")
-	dotest("--+++-+-", "+++++---", "00+++-0-")
-	dummy = input('Press ENTER ...')
-
+  dotest("--++--", "++--++", "000000")
+  dotest("-+-+-+", "-+-+-+", "-+-+-+")
+  dotest("-++-", "-+-+", "-+00")
+  dotest("--++", "++++", "00++")
+  dotest("+++--+---", "++----++-", "++0--000-")
+  dotest("-----", "-----", "-----")
+  dotest("-+", "++", "0+")
+  dotest("--", "-+", "-0")
+  dotest("-++", "+--", "000")
+  dotest("++-++--++-", "-+++-++-++", "0+0+0000+0")
+  dotest("-++-+-++-", "+-++++---", "00+0+000-")
+  dotest("---++-+--", "-+++--++-", "-00+0-+0-")
+  dotest("+-----+++-", "--+-+-++--", "0-0-0-++0-")
+  dotest("+-----+-", "---++-++", "0--00-+0")
+  dotest("-+--+-+---", "-+--+-+-+-", "-+--+-+-0-")
+  dotest("+-+", "-++", "00+")
+  dotest("-++", "-+-", "-+0")
+  dotest("---+", "-+++", "-00+")
+  dotest("+--", "+--", "+--")
+  dotest("--+++-+-", "+++++---", "00+++-0-")
 
 if __name__ == "__main__":
-	main()
+  main()
