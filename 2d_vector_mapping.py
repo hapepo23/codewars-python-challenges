c055e9a3ed09a6667d3a37dbced84515d3a254e2
hapepo23 <238172935+hapepo23@users.noreply.github.com>
2026-03-17 17:14:58 +0100

    update 2026-03-17 17:14 CET

--- 2d_vector_mapping.py
+++ 2d_vector_mapping.py
@@ -16,31 +16,30 @@ def map_vector(vector, circle1, circle2):
   r2 = r1 * circle2[2] / circle1[2]
   x2 = r2 * math.cos(a) + circle2[0]
   y2 = r2 * math.sin(a) + circle2[1]
-  return [x2, y2]
+  return (x2, y2)
 
 # ---- SECOND SOLUTION ----
 '''
 def map_vector(vector, circle1, circle2):
-  return [circle2[0] + circle2[2] / circle1[2] * (vector[0] - circle1[0]),
-          circle2[1] + circle2[2] / circle1[2] * (vector[1] - circle1[1])]
+  return (circle2[0] + circle2[2] / circle1[2] * (vector[0] - circle1[0]),
+          circle2[1] + circle2[2] / circle1[2] * (vector[1] - circle1[1]))
 '''
 
 # ---- TEST ----
 
 def dotest(vector, circle1, circle2, expected):
   actual = map_vector(vector, circle1, circle2)
-  actual[0] = round(actual[0], 2)
-  actual[1] = round(actual[1], 2)
+  actual = (round(actual[0], 2), round(actual[1], 2))
   status = 'OK' if expected == actual else 'FAIL'
   print(
     f'Vector = {vector}, circle1 = {circle1}, circle2 = {circle2}\nexpected = {expected}, actual = {actual} -> {status}\n')
 
 def main():
-  dotest([46, 58], [0, 0, 100], [0, 0, 100], [46, 58])
-  dotest([50, 88], [-25, 128, 100], [50, 50, 100], [125, 10])
-  dotest([120, 58], [100, 76, 36], [120, -62, 50], [147.78, -87.0])
-  dotest([5, 5], [10, 20, 42], [-100, -42, 60], [-107.14, -63.43])
-  dotest([5, 5], [10, 20, 42], [-100, -42, 69], [-108.21, -66.64])
+  dotest((46, 58), (0, 0, 100), (0, 0, 100), (46, 58))
+  dotest((50, 88), (-25, 128, 100), (50, 50, 100), (125, 10))
+  dotest((120, 58), (100, 76, 36), (120, -62, 50), (147.78, -87.0))
+  dotest((5, 5), (10, 20, 42), (-100, -42, 60), (-107.14, -63.43))
+  dotest((5, 5), (10, 20, 42), (-100, -42, 69), (-108.21, -66.64))
 
 if __name__ == "__main__":
   main()
