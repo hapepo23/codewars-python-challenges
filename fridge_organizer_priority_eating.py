6706881f4ec280f47e61b3a0e9c5d93772c867a6
hapepo23 <238172935+hapepo23@users.noreply.github.com>
2026-03-18 15:48:34 +0100

    update 2026-03-18 15:48 CET

---
+++ fridge_organizer_priority_eating.py
@@ -0,0 +1,45 @@
+'''
+7 kyu
+Fridge Organizer: Priority Eating
+https://www.codewars.com/kata/69971f385353edeaf428e1b0
+'''
+
+# ---- SOLUTION ----
+
+def fridge_organizer(items):
+  it = [row for row in items if row.expiry_days >= 0]
+  it.sort(key=lambda e: (not e.is_almost_empty, e.expiry_days, e.name))
+  return [row.name for row in it]
+
+# ---- TEST ----
+
+class FoodItem:
+  def __init__(self, n, ed, iae):
+    self.name = n
+    self.expiry_days = ed
+    self.is_almost_empty = iae
+
+  def tostring(self):
+    return '[' + self.name + ', ' + str(self.expiry_days) + ', ' + str(self.is_almost_empty) + ']'
+
+def dotest(items, expected):
+  actual = fridge_organizer(items)
+  status = 'OK' if expected == actual else 'FAIL'
+  print(f'Items = {[items[row].tostring() for row in range(len(items))]}')
+  print(f'expected = {expected}, actual = {actual} -> {status}\n')
+
+def main():
+  dotest(
+    [FoodItem("Milk", 3, False),
+     FoodItem("Jam", 3, True),
+     FoodItem("Yogurt", 1, False),
+     FoodItem("Old Meat", -1, True),
+     FoodItem("Today's Tofu", 0, False)],
+    ["Jam", "Today's Tofu", "Yogurt", "Milk"])
+  dotest(
+    [FoodItem("Zucchini", 5, False),
+     FoodItem("Apples", 5, False)],
+    ["Apples", "Zucchini"])
+
+if __name__ == "__main__":
+  main()
