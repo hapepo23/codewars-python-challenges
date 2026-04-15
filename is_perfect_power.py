4f3ef652b9447af52da0f1356d69e8a6a762e7b2
hapepo23 <238172935+hapepo23@users.noreply.github.com>
2026-04-15 22:31:00 +0200

    update 2026-04-15 22:30 CEST

---
+++ is_perfect_power.py
@@ -0,0 +1,36 @@
+'''
+5 kyu
+What's a Perfect Power anyway?
+https://www.codewars.com/kata/54d4c8b08776e4ad92000835/
+'''
+
+# ---- SOLUTION ----
+
+import math
+
+def isPP(n):
+  if n < 2:
+    return None
+  max_b = int(math.log2(n)) + 1
+  for b in range(2, max_b + 1):
+    a = int(round(n ** (1 / b)))
+    if a > 1 and a ** b == n:
+      return [a, b]
+  return None
+
+# ---- TEST ----
+
+def dotest(n, expected):
+  actual = isPP(n)
+  status = 'OK' if expected == actual else 'FAIL'
+  print(f'n = {n}, expected = {expected}, actual = {actual} -> {status}')
+
+def main():
+  dotest(4, [2, 2])
+  dotest(9, [3, 2])
+  dotest(5, None)
+  dotest(961, [31, 2])
+  dotest(4096, [64, 2])
+
+if __name__ == "__main__":
+  main()
